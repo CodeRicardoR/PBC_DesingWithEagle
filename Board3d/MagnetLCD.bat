05e4d3cdb46bf6dd9d2c97794b09b0c30a31637b
Ricardo Rojas (WIN10-2) <net85.ricardo@gmail>
2023-11-09 11:56:46 -0500

    Uploading cktos

---
+++ Board3d/MagnetLCD.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"pvengine.exe" +W1600 +H1200 +A0.3 +FN +L"c:/windows/fonts" +L"C:/Program Files (x86)/EAGLE-5.11.0/eagle3d/ulp//../povray" +O"C:/Users/Usuario/Documents/eagle/Board3d/MagnetLCD.png" /RENDER "C:/Users/Usuario/Documents/eagle/Board3d/MagnetLCD.pov" 
